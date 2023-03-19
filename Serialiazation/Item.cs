423cb983874ddfb6d799bcea9a9dde78aa51f120
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-03-19 18:20:27 +0200

    Add project files.

---
+++ Serialiazation/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace Serialiazation
+{
+    public class Item
+    {
+        public Item() { }
+        public Item(string name, string producer, double price)
+        {
+            this.Name = name;
+            this.Producer = producer;
+            this.Price = price;
+        }
+        [XmlAttribute]
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Serialiazation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialiazation
+{
+    [XmlRoot("Store")]
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializer = new XmlSerializer(typeof(Item));
+            using(var wr = new StreamWriter("ItemInfo.xml"))
+            {
+                serializer.Serialize(wr, new Item() { Name="ItemName", Price = 2000, Producer= "ItemProducer" });
+            };
+        }
+    }
+}
